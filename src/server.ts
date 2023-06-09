7f3965533f90935dc457f4068779ca56c8e62738
alnomancs <alnoman.cs@gmail.com>
2023-06-09 09:31:57 +0100

    shlasd

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Database connection established");
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connection established')
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Failed to run", error);
+    console.log('Failed to run', error)
   }
 }
 
-main();
+main()
